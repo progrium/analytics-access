ad4e8de8eb8ded27e2d7b629db01fefd80c0895c
Jeff Lindsay <progrium@gmail.com>
2016-08-14 11:21:55 -0500

    initial commit

---
+++ api.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+import os
+import base64
+from flask import Flask, jsonify
+from oauth2client.service_account import ServiceAccountCredentials
+
+app = Flask(__name__)
+
+# The scope for the OAuth2 request.
+SCOPE = 'https://www.googleapis.com/auth/analytics.readonly'
+
+# Defines a method to get an access token from the ServiceAccount object.
+def get_access_token():
+  return ServiceAccountCredentials.from_json_keyfile_name(
+      "json-key.json", SCOPE).get_access_token().access_token
+
+@app.route("/token")
+def token():
+    return jsonify(**{"token": get_access_token()})
+
+if __name__ == "__main__":
+    with open("json-key.json", "w") as f:
+        f.write(base64.b64decode(os.environ.get("JSON_KEY")).decode('UTF-8'))
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
